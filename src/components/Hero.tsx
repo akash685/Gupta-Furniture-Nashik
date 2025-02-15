129c1ac966c2b79e6845055bdc73538a31b379c4
akash <akash96237@gmail.com>
2025-02-15 20:22:53 +0530

    fix css

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,11 +18,11 @@ function Hero() {
           />
         </div>
         <div className="container mx-auto px-6 z-10 flex flex-col md:flex-row items-center justify-between">
-          <div className="text-white md:w-1/2 text-center md:text-left mb-8 md:mb-0">
-            <h1 className=" text-3xl md:text-7xl font-bold  ">
+          <div className="text-white md:w-1/2 text-center md:text-left md:mb-0">
+            <h1 className=" text-lg pb-5 md:text-7xl font-bold  ">
               Best Furniture & Interior Design in Nashik
             </h1>
-            <p className="text-lg md:text-xl mb-6 leading-relaxed">
+            <p className="text-lg md:text-xl mb-6 leading-relaxed hidden ">
               Premium furniture and interior design solutions in Nashik
             </p>
             <Link
